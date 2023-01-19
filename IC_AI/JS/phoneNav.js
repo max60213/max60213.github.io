5b15d059865ff4a18283b73b461d60e1a1b1575c
max60213 <96217023+max60213@users.noreply.github.com>
2023-01-19 19:41:35 +0800

    Delete IC_AI directory

--- IC_AI/JS/phoneNav.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector(".hamburger");
-const list = document.querySelector(".nav");
-
-hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("active");
-    list.classList.toggle("active");
-})
